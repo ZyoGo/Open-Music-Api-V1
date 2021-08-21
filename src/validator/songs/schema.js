ecff22a0cdea4f1ebd14f2424158b2681cde2b6d
w33h <academyynetworking@gmail.com>
2021-08-21 08:20:23 +0800

    Add specific validate data

--- src/validator/songs/schema.js
+++ src/validator/songs/schema.js
@@ -2,7 +2,8 @@ import Joi from 'joi';
 
 const SongsPayloadSchema = Joi.object({
   title: Joi.string().required(),
-  year: Joi.number().required(),
+  // eslint-disable-next-line newline-per-chained-call
+  year: Joi.number().integer().min(1900).max(2021).required(),
   performer: Joi.string().required(),
   genre: Joi.string(),
   duration: Joi.number(),
