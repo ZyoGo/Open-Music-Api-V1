9f764d4c06306a9857da42ff4385eda13dc3e22e
w33h <academyynetworking@gmail.com>
2021-10-18 21:22:48 +0800

    Add table playlistSong

--- migrations/1634209532280_create-table-playlistsong.cjs
+++ migrations/1634209532280_create-table-playlistsong.cjs
@@ -3,7 +3,7 @@
 exports.shorthands = undefined;
 
 exports.up = (pgm) => {
-  pgm.createTable('playlistsong', {
+  pgm.createTable('playlistsongs', {
     id: {
       type: 'VARCHAR(100)',
       primaryKey: true,
@@ -19,13 +19,13 @@ exports.up = (pgm) => {
   });
 
   //Add constraint UNIQUE for avoid duplication of data between the two values
-  pgm.addConstraint('playlistsong', 'unique_playlist_id_song_id', 'UNIQUE(playlist_id, song_id)');
+  pgm.addConstraint('playlistsongs', 'unique_playlist_id_song_id', 'UNIQUE(playlist_id, song_id)');
 
   //Add FK on column playlist_id and song_id to playlists.id and sons.id
-  pgm.addConstraint('playlistsong', 'FK_playlistsong.playlist_id.playlists.id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(id)');
-  pgm.addConstraint('playlistsong', 'FK_playlistsong.song_id.songs.id', 'FOREIGN KEY(song_id) REFERENCES songs(id)');
+  pgm.addConstraint('playlistsongs', 'FK_playlistsong.playlist_id.playlists.id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(id)');
+  pgm.addConstraint('playlistsongs', 'FK_playlistsong.song_id.songs.id', 'FOREIGN KEY(song_id) REFERENCES songs(id)');
 };
 
 exports.down = (pgm) => {
-  pgm.dropTable('playlistsong');
+  pgm.dropTable('playlistsongs');
 };
